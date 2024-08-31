7d4e819eafee8c315d1376cc2d0e1e506400764b
wasif12334 <kashifaliraza95@gmail.com>
2024-08-31 15:06:11 +0500

    Added file

---
+++ list is or opt.py
@@ -0,0 +1,12 @@
+# # Is OR  ISnOt Identity operators
+# a= [1,2,3]
+# b=a
+# print(a is b )
+# c= b
+# print (c is  b)
+
+
+freinds = [ "Wasif ","Kashif ","Zain "]
+
+for freind in freinds:
+    print(f"Hi {freind} welcome")
